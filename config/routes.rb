f243409ec1d1781b5883fcda7f233680e5b0c3d1
naru-1077 <t.n.narunaru@gmail.com>
2021-04-02 06:36:13 +0000

    [Add]モデル、コントローラ、ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,31 @@ Rails.application.routes.draw do
   passwords:     'users/passwords',
   registrations: 'users/registrations'
 }
+
+namespace :admin do
+    get "/" => "users#index"
+    resources :users, only: [:index, :show, :edit, :update]
+
+    resources :genres, only: [:index, :create, :edit, :update]
+
+  end
+
+scope module: :user do
+    root to: "homes#top"
+    get "/about" => "homes#about"
+    get "/users/my_page" => "users#show"
+    get "/users/unsubscribe" => "users#unsubscribe"
+    patch "/users/withdraw" => "users#withdraw"
+    get "/users/edit" => "users#edit"
+    patch "/users" => "users#update"
+
+
+    resources :favorites, only: [:index, :create, :destroy]
+
+    resources :post_comments, only: [:index, :new, :create, :edit, :update, :destroy]
+
+    resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_02_044519) do
+ActiveRecord::Schema.define(version: 2021_04_02_055854) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,46 @@ ActiveRecord::Schema.define(version: 2021_04_02_044519) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "genre_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "image_id"
+    t.string "title"
+    t.text "introduction"
+    t.text "material"
+    t.integer "user_id"
+    t.integer "genre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "recipe_image_id"
+    t.text "recipe"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
