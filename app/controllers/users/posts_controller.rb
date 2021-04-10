efbf52971e465de61cf318378bf6268d7ce7b779
naru-1077 <t.n.narunaru@gmail.com>
2021-04-10 06:26:17 +0000

    [Add]検索機能

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -5,6 +5,8 @@ class Users::PostsController < ApplicationController
     @posts_all = Post.all
     @genre = Genre.find(params[:id]) if params[:id]
     @genres = Genre.all
+    @q = Post.ransack(params[:q])
+    @posts_search = @q.result(distinct: true)
   end
 
   def new
@@ -50,10 +52,21 @@ class Users::PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def search
+    @q = Post.search(search_params)
+    @posts_search = @q.result(distinct: true)
+    @posts = Post.page(params[:page]).per(6)
+    @genre = Genre.find(params[:id]) if params[:id]
+    @genres = Genre.all
+  end
 
   private
   def post_params
       params.require(:post).permit(:image, :title, :introduction, :material, :genre_id, recipes_attributes: [:recipe_image, :recipe])
+  end
+
+  def search_params
+      params.require(:q).permit(:title_or_material_cont, :genre_id_eq)
   end
 
 end
