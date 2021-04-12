c3c437c28e244a4f7b45184e4f52c279cd5ca170
naru-1077 <t.n.narunaru@gmail.com>
2021-04-12 05:00:57 +0000

    [Add]タグ機能修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,11 @@
 //
 
 //= require jquery3
+//= require tagsinput
 //= require popper
 //= require bootstrap-sprockets
 
 //= require rails-ujs
-//= require jquery-ui
-//= require tag-it
 //= require activestorage
 // require turbolinks
 //= require_tree .
@@ -111,23 +110,4 @@ reader.onload = function (e) {
 }
 reader.readAsDataURL(e.target.files[0]);
 });
-});
-
-// ページ更新でtag-it発火
-$(document).ready(function() {
-  $(".tag_form").tagit({  // 指定のセレクタ( 今回は、:tag_list の text_field )に、tag-itを反映
-    tagLimit:10,         // タグの最大数
-    singleField: true,   // タグの一意性
- // availableTags: ['ruby', 'rails', ..]  自動補完する一覧を設定できる(※ 配列ならok)。今回は、Ajax通信でDBの値を渡す(後述)。
-  });
-  let tag_count = 10 - $(".tagit-choice").length    // 登録済みのタグを数える
-  $(".ui-widget-content.ui-autocomplete-input").attr(
-    'placeholder','あと' + tag_count + '個登録できます');
-})
-
-// タグ入力で、placeholder を変更
-$(document).on("keyup", '.tagit', function() {
-  let tag_count = 10 - $(".tagit-choice").length    // ↑ と同じなので、まとめた方がいい。
-  $(".ui-widget-content.ui-autocomplete-input").attr(
-  'placeholder','あと' + tag_count + '個登録できます');
 });
