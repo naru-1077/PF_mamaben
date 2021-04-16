7261743016e0a5ab780acb69fd8c7b2de2e41952
naru-1077 <t.n.narunaru@gmail.com>
2021-04-16 08:40:53 +0000

    [Add]ページトップとスライド機能

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -111,3 +111,63 @@ reader.onload = function (e) {
 reader.readAsDataURL(e.target.files[0]);
 });
 });
+
+
+// (ページトップのスクロール)
+//スクロールした際の動きを関数でまとめる
+$(document).on('turbolinks:load', function () {
+
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 100){//上から100pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+	var scroll = $(window).scrollTop(); //スクロール値を取得
+	if(scroll > 0){
+		$(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
+        $('body,html').animate({
+            scrollTop: 0
+        }, 2000,function(){//スクロールの速さ。数字が大きくなるほど遅くなる
+            $('#page-top').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
+        });
+	}
+    return false;//リンク自体の無効化
+});
+});
+
+// (ページトップのslick)
+$(document).on('turbolinks:load', function(){
+  $('.slick').slick({
+    autoplay:true,  //自動再生
+    dots:true,      //ドットの描写
+    accessibility: true,  // アクセシビリティ。左右ボタンで画像の切り替えをできるかどうか
+    autoplaySpeed: 3000,  // 自動再生で切り替えをする時間
+    speed: 1000,  // 自動再生や左右の矢印でスライドするスピード
+    pauseOnHover: true,  // 自動再生時にスライドのエリアにマウスオンで一時停止するかどうか
+    pauseOnDotsHover: true,  // 自動再生時にドットにマウスオンで一時停止するかどうか
+    arrows: true,  // 左右の次へ、前へボタンを表示するかどうか
+    infinite: true,  // 無限スクロールにするかどうか。最後の画像の次は最初の画像が表示される。
+  });
+});
--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -1,5 +1,7 @@
 class Users::FavoritesController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @favorites = current_user.favorite_posts.page(params[:page]).per(6)
   end
