9c8197e566d820c472bd8c9b3ffe735f7d79baac
naru-1077 <t.n.narunaru@gmail.com>
2021-04-21 02:44:49 +0000

    [Add]レイアウト全般調整

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -3,7 +3,7 @@ class Admins::UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.page(params[:page])
+    @users = User.page(params[:page]).per(20)
   end
 
   def show
